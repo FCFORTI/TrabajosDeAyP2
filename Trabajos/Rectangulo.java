d387df8629f7f6e3c4055090feed80f8270ff361
Franco Forti <48494992+FCFORTI@users.noreply.github.com>
2019-03-24 23:37:26 -0300

    Clase Figura implementada

---
+++ Trabajos/Rectangulo.java
@@ -0,0 +1,14 @@
+public class Rectangulo extends Figura{
+	
+	private double base, altura;
+	
+	public Rectangulo(double base, double altura){
+		this.base = base;
+		this.altura = altura;
+	}
+	
+	public double area(){
+		return base*altura;
+	}
+
+}
